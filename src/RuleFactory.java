eb0e62a74c1ffc66f39b40c28ecbad952fd63133
suskin <suskindj@gmail.com>
2013-01-22 22:55:14 -0800

    Adding empty factory class for parsing rules

---
+++ src/RuleFactory.java
@@ -0,0 +1,15 @@
+import java.io.InputStream;
+import java.util.concurrent.ConcurrentNavigableMap;
+
+/**
+ * Parsing rules from a file can be a pain, so we want some code to make it
+ * easier.
+ * 
+ * @author dsuskin
+ * 
+ */
+public class RuleFactory {
+
+    public ConcurrentNavigableMap<String, Rule> createRules(InputStream in) {
+    }
+}
