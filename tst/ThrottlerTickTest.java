3ca04cc3f31bd697b71eb9cec9b3c1f45e25b227
suskin <suskindj@gmail.com>
2013-02-13 22:50:18 -0800

    Adding ThrottlerTickTest class

---
+++ tst/ThrottlerTickTest.java
@@ -0,0 +1,29 @@
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * 
+ * @author dsuskin
+ * 
+ */
+public class ThrottlerTickTest {
+
+    private Throttler throttler;
+
+    @Before
+    public void setUp() {
+
+    }
+
+    @After
+    public void tearDown() {
+
+    }
+
+    @Test
+    public void shouldAddTokensInBackgroundThread() {
+        // TODO should use Mockito for this
+        throw new UnsupportedOperationException("Test not implemented");
+    }
+}
