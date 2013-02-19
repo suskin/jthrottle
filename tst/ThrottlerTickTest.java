b23df92751c80ac6ac9fedb1d13e74840a382807
suskin <suskindj@gmail.com>
2013-02-18 22:27:58 -0800

    Implementing ThrottlerTick test

--- tst/ThrottlerTickTest.java
+++ tst/ThrottlerTickTest.java
@@ -1,6 +1,9 @@
+import java.util.Timer;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mockito;
 
 /**
  * 
@@ -10,20 +13,33 @@ import org.junit.Test;
 public class ThrottlerTickTest {
 
     private Throttler throttler;
+    private ThrottlerTick throttlerTick;
 
     @Before
     public void setUp() {
-
+        throttler = Mockito.mock(Throttler.class);
+        throttlerTick = new ThrottlerTick(throttler);
     }
 
     @After
     public void tearDown() {
-
+        throttler = null;
+        throttlerTick = null;
     }
 
     @Test
-    public void shouldAddTokensInBackgroundThread() {
-        // TODO should use Mockito for this
-        throw new UnsupportedOperationException("Test not implemented");
+    public void shouldAddTokensInBackgroundThread() throws Exception {
+        long period = 500;
+        int desiredInvocations = 2;
+        // sleep for one period fewer than desired invocations because
+        // the first invocation happens at time 0
+        long testSleepTime = period * (desiredInvocations - 1) + period / 2;
+        Timer timer = new Timer();
+        timer.scheduleAtFixedRate(throttlerTick, 0, period);
+        Thread.sleep(testSleepTime);
+
+        timer.cancel();
+
+        Mockito.verify(throttler, Mockito.times(desiredInvocations)).tick();
     }
 }
